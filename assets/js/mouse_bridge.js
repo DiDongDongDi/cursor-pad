cc89ee886f3e964f3eed94a44d901cb58dd64453
kodyqywang <kodyqywang@tencent.com>
2026-07-02 12:54:56 +0800

    Revert "Refactor mouse_bridge.js to simplify return value in selection logic"
    This reverts commit 37994cf37ef27afe3e52a82830d55010587cd74a.

--- assets/js/mouse_bridge.js
+++ assets/js/mouse_bridge.js
@@ -1064,7 +1064,12 @@
         }),
       );
 
-      return selectWordAtPoint(lastX, lastY);
+      return {
+        x: lastX,
+        y: lastY,
+        tag: target ? target.tagName : null,
+        actionable: actionable ? actionable.tagName : null,
+      };
     },
 
     scroll: function (deltaX, deltaY) {
